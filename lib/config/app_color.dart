3581cf9aece6710058fa680b2b3b804124c22a1d
BenjaminHung8405 <nphung8405@gmail.com>
2025-01-07 11:46:01 +0700

    Add Color define

---
+++ lib/config/app_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static final Color primaryColor = Color(0xffa67b5b);
+  static final Color backgroundColor = Color(0xffffffff);
+
+  static final Color boxColor = Color(0xffe8d3c5);
+
+  static final Color highLightColor = Color(0xffc8a27e);
+  static final Color hintColor = Color(0xffd4b59e);
+}
